3fbe6cfd9f4eb1946835a20ee12dac4eac1a20b3
luuphuonguyen <len.kagamine181998@gmail.com>
2020-06-13 17:34:26 +0700

    a

---
+++ lib/screen/show_applicant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:project06/model/applicant.dart';
+class ShowApplicant extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
